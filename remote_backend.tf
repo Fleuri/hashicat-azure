64edafc36d10cda6d3d7c9f87958f91c899833fd
Fleuri <laur1.suomalainen@gmail.com>
2022-03-21 16:21:51 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "Fleuri-training"
     workspaces {
       name = "hashicat-azure"
